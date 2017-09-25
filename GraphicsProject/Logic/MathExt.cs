1e2224ca10f08d0d8b872b3b3b32e2992dc1a1ed
kihen0 <kirill.morozowhts@gmail.com>
2017-09-25 12:57:22 +0300

    Added Matrix Algebra

--- GraphicsProject/Logic/MathExt.cs
+++ GraphicsProject/Logic/MathExt.cs
@@ -15,15 +15,45 @@ namespace Logic
             {
                 data = inpMatrix;
             }
-            /*double Det()
+            public double[,] Multiply(double[,] matrixB)
             {
-                if (data.GetLength(1) != data.GetLength(0))
-                    throw new Exception("!!!!");
-                for (int i = 0; i < data.GetLength(1); i++)
+                if (matrixB.GetLength(0) != this.data.GetLength(1))
+                    throw new Exception("!!!");
+                double[,] result = new double[data.GetLength(0),matrixB.GetLength(1)];
+                for (int i = 0; i <data.GetLength(0); i++)
                 {
-
+                    for (int j = 0; j <matrixB.GetLength(1); j++)
+                    {
+                        double sum=0;
+                        for (int k = 0; k < this.data.GetLength(1); k++)
+                        {
+                            sum += data[i, k] * matrixB[k, j];
+                        }
+                        result[i,j] = sum;
+                    }
                 }
-            }*/
+                return result;
+            }
+            public void MultiplyByNumber(double number)
+            {
+                for (int i = 0; i < data.GetLength(0); i++)
+                {
+                    for (int j = 0; j < data.GetLength(1); j++)
+                    {
+                        data[i, j] *= number;
+                    }
+                }
+            } 
+            public static Matrix IdentityMatrix(int size)
+            {
+                double[,] result = new double[size, size];
+                for (int i = 0; i < size; i++)                
+                    for (int j = 0; j < size; j++)                    
+                        if (i==j)                       
+                            result[i, j] = 1;
+
+                return new Matrix( result);                                      
+            }
         }
     }
 }
---
+++ GraphicsProject/Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathExt.Matrix a = new MathExt.Matrix(new double[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } });
+            double[,] edin = new double[3, 3] { { 2,1,3 }, { 5,4,6 }, { 8,7,9 } };
+            var matr = a.Multiply(edin);
+            for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr.GetLength(1); j++)
+                {
+                    Console.Write(" "+matr[i, j]); 
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
